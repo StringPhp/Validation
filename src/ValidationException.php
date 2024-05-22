31f5f4408cd4cb2c5f807ea623f1a0afc9ce0f66
Robert Snedeker <rsnedeker20@gmail.com>
2024-05-22 08:20:38 -0400

    Error list must be supplied to ValidationException

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -6,4 +6,9 @@ use Exception;
 
 class ValidationException extends Exception
 {
+    public function __construct(
+        public readonly array $errors
+    ) {
+        parent::__construct();
+    }
 }
